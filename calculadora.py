3e339c49586187116d382cf596436547f0750790
jhames1208 <jhamesflores589@gmail.com>
2026-07-22 16:06:08 -0500

    Update calculadora.py
    Agregar la multiplicacion
    Jhames Flores

--- calculadora.py
+++ calculadora.py
@@ -6,6 +6,7 @@ num2 = float(input("Ingrese el segundo número: "))
 
 print("\n 1. Sumar")
 print(" 2. Restar")
+print(" 3. Multiplicar")
 
 opcion = input("\n Seleccione una opción: ")
 
@@ -14,6 +15,9 @@ if opcion == "1":
 
 elif opcion == "2":
     print("\n La resta es:", num1 - num2)
+
+elif opcion == "3":
+    print("\n La multiplicación es:", num1 * num2)
 
 else:
     print("\n Opción no válida.")
